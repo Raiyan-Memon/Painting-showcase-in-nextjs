de2614edc4c7ec9b0fdf91f30b4969577ec29035
Raiyan Memon <raiyan.appdid@gmail.com>
2023-07-26 16:14:18 +0530

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
                     </div> */}
                     <div className="col-md-12">
                         <h3 className='text-center mt-4'>I am <span>Manasi</span></h3>
-                        <h6 className='mt-2'>I am a Computer Science Engineer with a passion of Painting. Through this Website I am showcasing my Paintings which are painted by me itself.</h6>
+                        <h6 className='mt-2'>Drawing things makes them seem more real and makes me feel more alive.</h6>
 
                         <div className="row mt-3  justify-content-center">
                         <div className="col-md-1 col-2">
